882e614c856d90fa2ffe4675aa9a680310c7a45a
Abhilasha-SOU <abilashasoundar2@gmail.com>
2026-04-27 16:50:28 +0530

    Fix firebaseConfig import path

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -5,7 +5,7 @@ import mail_icon from "../../assets/mail_icon.svg";
 import call_icon from "../../assets/call_icon.svg"
 import location_icon from "../../assets/location_icon.svg";
 import { ref, push } from "firebase/database";
-import { db } from "../../firebaseconfig";
+import { db } from "../../firebaseConfig";
 
 
 // const Contact = () => {
